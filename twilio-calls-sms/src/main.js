96f4822bd7f5e7a3406250c9163436d0902e1298
himelsaha29 <himelsaha29@live.com>
2022-01-29 11:54:12 -0500

    Main.js updated with AuthPlugin

--- twilio-calls-sms/src/main.js
+++ twilio-calls-sms/src/main.js
@@ -1,16 +1,29 @@
-import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
-import App from './App'
+import Vue from "vue";
+import App from "./App.vue";
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
 
-/* eslint-disable no-new */
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
+Vue.config.productionTip = false;
+
 new Vue({
-  el: '#app',
   router,
   render: h => h(App)
-})
+}).$mount("#app");
